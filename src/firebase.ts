8e8776abe042b84e60f12b55fd56db980596a61d
tikhomirowww <tihomirovi92@gmail.com>
2023-11-22 20:33:27 +0600

    .

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,14 +1,9 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { lstat } from "fs";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-// REACT_APP_APIKEY = AIzaSyAYxEGV2IpmoP9yLmrblfccXFbiyJB-SbU
-// REACT_APP_AUTHDOMAIN = ev-16-6c056.firebaseapp.com
-// REACT_APP_PROJECTID = ev-16-6c056
-// REACT_APP_STORAGEBUCKET = ev-16-6c056.appspot.com
-// REACT_APP_MESSAGINGSENDERID = 844494034763
-// REACT_APP_APPID
 // Your web app's Firebase configuration
 
 const firebaseConfig = {
